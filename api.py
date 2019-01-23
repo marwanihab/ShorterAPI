9471441a181067af3e73c72d3f0da15842a6356c
marwanihab <marwan.ihab95@gmail.com>
2019-01-23 02:15:15 +0200

    Done with schemas validation

--- api.py
+++ api.py
@@ -7,6 +7,8 @@ from pymongo import MongoClient
 from flask import make_response
 import random, string
 from flask_api import status # HTTP Status Codes
+import schemas
+from jsonschema.exceptions import ValidationError 
 
 
 ######################################################################
@@ -38,10 +40,15 @@ def add_shortlink(db,shortlink):
     db.links.insert_one(shortlink)
 
 
-    
+@app.errorhandler(ValidationError)
+def request_schema_error(e):
+    return make_response(jsonify(status=400, error='Bad Request', message=str(e)), status.HTTP_400_BAD_REQUEST)
+
+
 
 @app.errorhandler(DataValidationError)
 def request_validation_error(e):
+
     return make_response(jsonify(status=400, error='Bad Request', message=str(e)), status.HTTP_400_BAD_REQUEST)
 
 @app.errorhandler(404)
@@ -50,14 +57,16 @@ def not_found(e):
                          "status": "failed",
                          "message": "not found"
                         }
+    schemas.validate_client_error_schema(response)                    
     return make_response(jsonify(response), status.HTTP_404_NOT_FOUND)
 
 @app.errorhandler(400)
 def bad_request(e):
     response = {
                           "status": "failed",
-                          "message": "Bad Request you have provided already existed slug ID"
+                          "message": "Bad Request"
                          }
+    schemas.validate_client_error_schema(response)                     
     return make_response(jsonify(response), status.HTTP_400_BAD_REQUEST)
 
 @app.errorhandler(405)
@@ -67,6 +76,7 @@ def method_not_allowed(e):
 @app.errorhandler(500)
 def internal_error(e):
  response = {}
+ schemas.validate_server_error_schema(response)
  return make_response(jsonify(response), status.HTTP_500_INTERNAL_SERVER_ERROR)
  
 
@@ -77,12 +87,17 @@ def get_add_handler():
         response =[]
         for link in db.links.find():
              response.append({'slug':link['slug'],'ios':link['ios'],'android':link['android'] , 'web':link['web']})
-
-        return jsonify({'shortlinks':response})
+        
+        output={}
+        output['shortlinks'] = response
+        schemas.validate_get_schema(output)
+        return jsonify(output)
     else :
         
         if not request.is_json:
             raise DataValidationError('Case Non-JSON Content-Type')
+        
+        schemas.validate_post_schema(request.get_json())
 
         slugID = request.json['slug']
         
@@ -90,7 +105,7 @@ def get_add_handler():
                 slugID = generate_random_slug()
 
         if get_shortlink(db,slugID):
-            abort(400)         
+            raise DataValidationError('You have entered an already existed slug,please provide a unique slug')       
         else :
          ios = request.json['ios']
          android = request.json['android']
@@ -101,14 +116,14 @@ def get_add_handler():
              "android": android,
              "web": web
                      }
-
+        
          add_shortlink(db,shortlink)
          response = {
              "status": "successful",
              "slug": slugID,
              "message": "created successfully"
                     } 
-
+         schemas.validate_post_response_schema(response)
          return  jsonify(response)
 
 
@@ -119,37 +134,44 @@ def update_handler(slug):
             raise DataValidationError('Case Non-JSON Content-Type')
 
     if not get_shortlink(db,slug):
-            abort(400)       
+            raise DataValidationError('Please enter a valid slug')    
     
+    schemas.validate_put_schema(request.get_json())
+
     data = request.get_json()
     response={'slug':slug}
     if 'ios' in data: 
         ios = {} 
         if 'primary' in data['ios']:
             ios['primary'] = data['ios']['primary']
+            db.links.update_one({'slug':slug },{"$set": { 'ios.primary':ios['primary'] } })
         if 'fallback' in data['ios']:
             ios['fallback'] = data['ios']['fallback']
+            db.links.update_one({'slug':slug },{"$set": { 'ios.fallback':ios['fallback'] } })
         if not len(ios) == 0:
          response['ios']= ios       
-         db.links.update_one({'slug':slug},{"$set": { 'ios': ios} })
+         
 
     if 'android' in data:
         android = {} 
         if 'primary' in data['android']:
             android['primary'] = data['android']['primary']
+            db.links.update_one({'slug':slug},{"$set": { 'android.primary': android['primary']} })
         if 'fallback' in data['android']:
             android['fallback'] = data['android']['fallback']
+            db.links.update_one({'slug':slug},{"$set": { 'android.fallback': android['fallback']} })
         if not len(android) == 0: 
          response['android']= android
-         db.links.update_one({'slug':slug},{"$set": { 'android': android} })     
+              
         
     if 'web' in data:
         web = data['web']
 
         if not len(web) == 0: 
          response['web']= web
-         db.links.update_one({'slug':slug},{"$set": { 'web': web} })    
+         db.links.update_one({'slug':slug},{"$set": { 'web': web} }) 
 
+    schemas.validate_put_response_schema(response)
     return jsonify(response)            
        
      
